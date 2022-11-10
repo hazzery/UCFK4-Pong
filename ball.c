8966ad4613a35ad3bca764118624dfb00405aa60
Harry Parkes <harrydparkes@protonmail.com>
2022-11-10 17:48:57 +1300

    Created ball movement

---
+++ ball.c
@@ -0,0 +1,45 @@
+/**
+ *  @file    ball.c
+ *  @authors Harrison Parkes
+ *  @date    10th of November 2022
+ *  @brief   ball physics for pong
+*/
+
+#include "../common/Position.h"
+#include "../common/vector.h"
+#include "../common/config.h"
+#include "paddle.h"
+#include "ball.h"
+
+static Vector_t ball_velocity = {.x = 1, .y = -1};
+static Pos_t ball_position = {.row = 0, .col = 1};
+
+extern bool is_playing;
+
+void ball_update()
+{
+    ball_position.col += ball_velocity.x;
+    ball_position.row += ball_velocity.y;
+
+    if (ball_position.col > GRID_WIDTH - 1) {
+        ball_position.col = GRID_WIDTH -1;
+    }
+    if (ball_position.row > GRID_HEIGHT - 1) {
+        ball_position.row = GRID_HEIGHT -1;
+    }
+
+    if (ball_position.row == GRID_HEIGHT - 1) {
+        if (paddle_collision(&ball_position)) {
+            ball_velocity.y *= -1;
+            ball_velocity.x += paddle_velocity();
+        } else {
+            is_playing = false;
+        }
+    } else if (ball_position.row == 0) {
+        ball_velocity.y *= -1;
+    }
+
+    if (ball_position.col == GRID_WIDTH - 1 || ball_position.col == 0) {
+        ball_velocity.x *= -1;
+    }
+}
---
+++ ball.h
@@ -0,0 +1,13 @@
+/**
+ *  @file    ball.h
+ *  @authors Harrison Parkes
+ *  @date    10th of November 2022
+ *  @brief   ball physics for pong
+*/
+
+#ifndef HARRY_PARKES_UCFK4_BALL_H
+#define HARRY_PARKES_UCFK4_BALL_H
+
+extern void ball_update();
+
+#endif //HARRY_PARKES_UCFK4_BALL_H
